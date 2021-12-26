84c6b3b7a32efa4861e83b641e0d1e1db5187d84
Zipexpo <nvtngan@hcmus.edu.vn>
2021-12-26 10:53:17 -0600

    Updates

---
+++ precache-manifest.16255dd9817a84b68550e16a082e20cf.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804853916d7ad16230229bda0a517319",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cecd914500d2df0cec1f",
+    "url": "./static/css/main.e36a480b.chunk.css"
+  },
+  {
+    "revision": "e328171b51571516206e",
+    "url": "./static/js/2.71d85ebc.chunk.js"
+  },
+  {
+    "revision": "cecd914500d2df0cec1f",
+    "url": "./static/js/main.50785224.chunk.js"
+  },
+  {
+    "revision": "a9f1d282f951fc950ad9",
+    "url": "./static/js/runtime~main.f9e61779.js"
+  },
+  {
+    "revision": "b0a49b6cb663eca681b4a3edc090feb4",
+    "url": "./static/media/ACM1.b0a49b6c.png"
+  },
+  {
+    "revision": "3d4d7535bb2162eac1783ea4cee7f2cd",
+    "url": "./static/media/ACM2.3d4d7535.png"
+  },
+  {
+    "revision": "55024ea8792f6df4414b19d145333373",
+    "url": "./static/media/ICS_stacked-transparent.55024ea8.gif"
+  },
+  {
+    "revision": "fc6bb204d46698e7991fea5f0d614aa3",
+    "url": "./static/media/IEEE_logo.fc6bb204.jpg"
+  },
+  {
+    "revision": "a7479450b598dae6ebea757d588dbdab",
+    "url": "./static/media/campus-entrance-fall.a7479450.png"
+  },
+  {
+    "revision": "58fb4e7bb68986ae7d93f9dbe73001e8",
+    "url": "./static/media/cloudbuslogo.58fb4e7b.png"
+  },
+  {
+    "revision": "877da40572cad8eb928ac7e970f674c1",
+    "url": "./static/media/cta-illustration.877da405.svg"
+  },
+  {
+    "revision": "83aeeffbd9e81cc263310ee737e97a2a",
+    "url": "./static/media/features-split-image.83aeeffb.svg"
+  },
+  {
+    "revision": "9977570bf62c532c5229e6c2a1d8967d",
+    "url": "./static/media/frame-icon.9977570b.svg"
+  },
+  {
+    "revision": "c35dd856ec8eb2b4f3d935b6552d33e9",
+    "url": "./static/media/header.c35dd856.jpg"
+  },
+  {
+    "revision": "e22ec14c8d912e8f793a37181e4eea5e",
+    "url": "./static/media/illustration-element-01-top.e22ec14c.svg"
+  },
+  {
+    "revision": "b5e6fc1a482e36c9b6e6141d9dd5d88a",
+    "url": "./static/media/illustration-element-01.b5e6fc1a.svg"
+  },
+  {
+    "revision": "c34c32b991559f9ffce5024da78094ee",
+    "url": "./static/media/illustration-element-02-top.c34c32b9.svg"
+  },
+  {
+    "revision": "1d92e1f3197b6578204b41b38fcb9546",
+    "url": "./static/media/illustration-element-02.1d92e1f3.svg"
+  },
+  {
+    "revision": "41f424139c61a5b5c0b56d7e7f2b3c7a",
+    "url": "./static/media/illustration-element-03.41f42413.svg"
+  },
+  {
+    "revision": "8d90cdb8c7b4d161c5efc5c52d261b1b",
+    "url": "./static/media/illustration-element-04.8d90cdb8.svg"
+  },
+  {
+    "revision": "cf0a1aa107a0479443066051c688f056",
+    "url": "./static/media/illustration-element-05.cf0a1aa1.svg"
+  },
+  {
+    "revision": "3cdb78d299b43be6a48a46163a6dd1dd",
+    "url": "./static/media/illustration-element-06.3cdb78d2.svg"
+  },
+  {
+    "revision": "36b12cc07e44922f8a1f7dd6115ad383",
+    "url": "./static/media/illustration-element-07.36b12cc0.svg"
+  },
+  {
+    "revision": "667ea8e055e7a1a1bc414e43190dde7d",
+    "url": "./static/media/illustration-element-08.667ea8e0.svg"
+  },
+  {
+    "revision": "38a7600ef5c6afb79f77355d284bef2c",
+    "url": "./static/media/illustration-section-01.38a7600e.svg"
+  },
+  {
+    "revision": "b5aed62027896ccbf1b4e9d143a94071",
+    "url": "./static/media/illustration-section-02.b5aed620.svg"
+  },
+  {
+    "revision": "80fdf350045b4310c3dde6508d5738ac",
+    "url": "./static/media/illustration-section-03.80fdf350.svg"
+  },
+  {
+    "revision": "22b7138b291f7f433189fd93d3f9ae03",
+    "url": "./static/media/illustration-section-04.22b7138b.svg"
+  },
+  {
+    "revision": "849d5654ae268da90ab975a0bafe4600",
+    "url": "./static/media/illustration-section-05.849d5654.svg"
+  },
+  {
+    "revision": "66abc7f824c1097641e9fd1bc7daceb2",
+    "url": "./static/media/illustration-section-06.66abc7f8.svg"
+  },
+  {
+    "revision": "af4c986b781b2d6ca89ecb9356e4e451",
+    "url": "./static/media/image-placeholder.af4c986b.png"
+  },
+  {
+    "revision": "9d1da83d3b4fa9ea3c89719f24ac449c",
+    "url": "./static/media/logo.9d1da83d.svg"
+  },
+  {
+    "revision": "9a00426ff68182bc626617dbaf2d639b",
+    "url": "./static/media/team-member-01.9a00426f.jpg"
+  },
+  {
+    "revision": "866e7db35fa53a58962df4e10d98ebbe",
+    "url": "./static/media/team-member-03.866e7db3.jpg"
+  },
+  {
+    "revision": "0a817cc6550885df4fe1ab4fc4224aa4",
+    "url": "./static/media/team-member-04.0a817cc6.jpg"
+  },
+  {
+    "revision": "2283ad00148b6684143c47877d55bff6",
+    "url": "./static/media/team-member-05.2283ad00.jpg"
+  },
+  {
+    "revision": "113234f84fb5199167037ded536e5dba",
+    "url": "./static/media/uol_logo.113234f8.jpg"
+  },
+  {
+    "revision": "22e4e6cd0ce1b7684d8aed4f99722b6b",
+    "url": "./static/media/video-placeholder.22e4e6cd.svg"
+  }
+]);
