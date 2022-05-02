fb8edbb52d580ad60f9a294760bbf7e7202347d6
Zipexpo <nvtngan@hcmus.edu.vn>
2022-05-02 04:22:06 -0500

    Updates

--- precache-manifest.74ce45b84b59d1e7b07dfad41f255f7f.js
+++ precache-manifest.74ce45b84b59d1e7b07dfad41f255f7f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "005d1af8482646e017f43269451f26e7",
+    "revision": "bbfc1dcd2dc36312d293561b09714a91",
     "url": "./index.html"
   },
   {
-    "revision": "2655f5c1ce90bdc03730",
+    "revision": "db1dacf55f15d66bc418",
     "url": "./static/css/main.e4ef614a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.1b941ffc.chunk.js"
   },
   {
-    "revision": "2655f5c1ce90bdc03730",
-    "url": "./static/js/main.160b24a3.chunk.js"
+    "revision": "db1dacf55f15d66bc418",
+    "url": "./static/js/main.cd5a37e8.chunk.js"
   },
   {
     "revision": "a9f1d282f951fc950ad9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9d822f2eec80e4724bad59719e2dc90a.js"
+  "./precache-manifest.74ce45b84b59d1e7b07dfad41f255f7f.js"
 );
 
 self.addEventListener('message', (event) => {
